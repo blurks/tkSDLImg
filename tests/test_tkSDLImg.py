8994204fcbb7a99b42471729dfdb792cd220d569
blurks <code@blurks.de>
2024-11-01 01:01:45 +0100

    improved testing

--- tests/test_tkSDLImg.py
+++ tests/test_tkSDLImg.py
@@ -1,7 +1,23 @@
+import numpy as np
 import pytest
-from typing import Tuple
+import tkinter as tk
+from typing import Generator, Tuple
 
 import tkSDLImg
+
+
+@pytest.fixture
+def tk_root() -> Generator[tk.Tk, None, None]:
+    root = tk.Tk()
+    yield root
+    root.destroy()
+
+
+@pytest.fixture
+def test_img() -> np.ndarray:
+    img = np.zeros((16, 16, 3), dtype=np.uint8)
+    img[8:, :, 2] = 255
+    return img
 
 
 @pytest.mark.parametrize(
@@ -42,3 +58,10 @@ def test_fit_dimenstions(
         expected: Tuple[int, int],
 ) -> None:
     assert tkSDLImg.fit_dimensions(src_size, dst_size) == expected
+
+
+def test_widget_ids(tk_root: tk.Tk, test_img: np.ndarray) -> None:
+    img_w1 = tkSDLImg.ImgWidget(tk_root, test_img)
+    img_w2 = tkSDLImg.ImgWidget(tk_root, test_img)
+    assert img_w1.widget.winfo_name() == "sdl0"
+    assert img_w2.widget.winfo_name() == "sdl1"
